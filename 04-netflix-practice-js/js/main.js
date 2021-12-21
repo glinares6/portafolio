5121c853765d38e1ff9549fd91f21892e78aed76
gianmarco <glinaresx@gmail.com>
2021-12-20 20:57:57 -0500

    netflix-carousel-scroll

--- 04-netflix-practice-js/js/main.js
+++ 04-netflix-practice-js/js/main.js
@@ -139,6 +139,7 @@ let step=10;
             // document.querySelector('.main__buttonind--activo').classList.remove('main__buttonind--activo')
 
             document.querySelector('.main__buttonind').classList.add('main__buttonind--activo')
+            fila.addEventListener('scroll',agre)
         }
 
 
@@ -191,7 +192,7 @@ const agre= ()=>{
 
 
 }
-fila.addEventListener('scroll',agre)
+
 
 
 //? ---------------------------- INICIALIZA LAS IMAGENES AL CARGAR -----------------------
