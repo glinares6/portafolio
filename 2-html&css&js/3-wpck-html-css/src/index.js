4ffd7ace8cb44404c6fb8cf34767aea9dc16200e
glinares6 <glinarese2@gmail.com>
2022-03-12 07:31:29 +0100

    webpack-scss-pug

--- 2-html&css&js/3-wpck-html-css/src/index.js
+++ 2-html&css&js/3-wpck-html-css/src/index.js
@@ -1,5 +1,5 @@
 // para que reconozca el css
-import "./styles.css";
+import "./styles.scss";
 const getPosts= async () =>{
     try{
         const response = await fetch("https://jsonplaceholder.typicode.com");
--- 2-html&css&js/3-wpck-html-css/webpack.config.js
+++ 2-html&css&js/3-wpck-html-css/webpack.config.js
@@ -10,46 +10,94 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // CSS
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+// const { minify } = require("css-minimizer-webpack-plugin/types/minify");
+
 
 module.exports = {
     // mode = modo de funcionamiento
         mode: "production",
     // entry point = donde empieza nuestra aplicacion
         entry: {
+            // app: ["@babel/polyfill","./src/index.js"] //
             app: "./src/index.js",
         },
 
         module: {
             rules:[
+             
+                { 
+					test: /\.pug$/, 
+					loader: "pug-loader",
+					// inject: true
+			},
+                  {
+                    test: /\.scss$/i,
+                    use: [
+                        // {
+                        //     loader: MiniCssExtractPlugin.loader,
+                        //     options:{
+                        //         hmr: process.env.NODE_ENV === 'development'
+                        //     }
+                        // },
+                        MiniCssExtractPlugin.loader,
+                        // "style-loader",
+                         "css-loader",
+                         "postcss-loader",
+                         "sass-loader"
+
+                        ]
+                },
+                  {
+                    test: /\.css$/i,
+                    use: [
+                        // {
+                        //     loader: MiniCssExtractPlugin.loader,
+                        //     options:{
+                        //         hmr: process.env.NODE_ENV === 'development'
+                        //     }
+                        // },
+                        MiniCssExtractPlugin.loader,
+                        // "style-loader",
+                         "css-loader",
+						 "postcss-loader",
+                        ]
+                },
                 {
                         test:/\.js/,
                         exclude: /node_modules/,
-                        use:["babel-loader"],
+                        use:[
+                            "babel-loader"
+                        ],
                     
-                },
-                {
-                    test: /\.css$/i,
-                    use: [MiniCssExtractPlugin.loader, "css-loader"]
                 },
             ],
         },
         plugins: [
+
             new HtmlWebpackPlugin({
-                minify: true,
-                scriptLoading: "blocking",
+                // en desarrollo
+                // scriptLoading: "blocking",
+                // filename: 'index.html',
+                template: './src/index.pug',
+				// inject: true,
+				// minify: false,
+                // title: 'Custom plantilla',
             }),
+
             new MiniCssExtractPlugin({
                 // para cambiar el nombre al archivo
-                    filename: "styles.css",
+                    filename: "styles1.css",
             }),
         ],
             // output point = salida del archivo
             output: {
                 path: path.join(basePath,distPath),
                filename: 'bundle.js',
-           },
-           optimization: {
-            minimizer: [new CssMinimizerPlugin()],
-            minimize: true,
-          },
+
+            },
+            // en modo desarrollo minifiza el css
+        //    optimization: {
+        //     minimizer: [new CssMinimizerPlugin()],
+        //     minimize: false,
+        //   },
 }
