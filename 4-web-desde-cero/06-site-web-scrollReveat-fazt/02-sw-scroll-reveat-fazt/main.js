a4cd1417cd01fb2976fa1f96c02393eabe80dff0
glinares05 <glinaresx@gmail.com>
2022-05-19 20:40:14 +0200

    scroll-reveal-orientacion

--- 4-web-desde-cero/06-site-web-scrollReveat-fazt/02-sw-scroll-reveat-fazt/main.js
+++ 4-web-desde-cero/06-site-web-scrollReveat-fazt/02-sw-scroll-reveat-fazt/main.js
@@ -7,3 +7,10 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', {delay:500, reset: true});
 ScrollReveal().reveal('.cards-banner-one', {delay:500, reset: true});
 ScrollReveal().reveal('.cards-banner-two', {delay:500, reset: true});
+
+ScrollReveal().reveal('.links', {  origin: 'bottom',
+interval: 106,
+duration: 2000,
+distance: '150%',
+reset: true});
+
---
+++ 4-web-desde-cero/06-site-web-scrollReveat-fazt/03-scroll-reveal-orientacion/script.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal()
+sr.reveal(".box1",{
+    origin: 'left',
+    interval: 106,
+    duration: 2000,
+    distance: '150%',
+    reset: true
+})
+sr.reveal(".box2",{
+    origin: 'top',
+    interval: 106,
+    duration: 2000,
+    distance: '150%',
+    reset: true
+
+})
