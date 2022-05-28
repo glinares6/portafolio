0482ce96c83a3447ac2d8acbcd4d79649127b581
glinares05 <glinaresx@gmail.com>
2022-05-28 02:39:26 +0200

    portafolio-json-exe

--- 4-web-desde-cero/08-portafolio/02-portafolio-dorian/js/scripts.js
+++ 4-web-desde-cero/08-portafolio/02-portafolio-dorian/js/scripts.js
@@ -5,6 +5,34 @@ const toggleText = document.getElementById('toggle-text');
 const toggleColor=document.getElementById('toggle-colors')
 const rootsStyle =document.documentElement.style
 
+const flagsElement=document.getElementById('flags');
+
+const textsToChange= document.querySelectorAll('[data-section]')
+
+const changeLanguage = async language => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+    // console.log(texts);
+
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section
+        const value = textToChange.dataset.value
+
+        // console.log(section, value);
+
+        textToChange.textContent=texts[section][value]
+    }
+  };
+
+
+
+flagsElement.addEventListener('click',(e) =>{
+    // console.log(e.target.parentElement.dataset.language);
+    changeLanguage(e.target.parentElement.dataset.language)
+
+})
+
+
 toggleTheme.addEventListener('click',() =>{
     document.body.classList.toggle('dark');
     if(toggleIcon.src.includes('moon.svg')){
