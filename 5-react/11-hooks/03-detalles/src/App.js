f65ddd5c073a5524b53008f3a95324783ecd499f
glinares6 <glinarese2@gmail.com>
2022-07-27 22:10:41 -0500

    react-proyect-11-v2

--- 5-react/11-hooks/03-detalles/src/App.js
+++ 5-react/11-hooks/03-detalles/src/App.js
@@ -1,15 +1,23 @@
 //* FUNCTIONAL COMPONENTS
 //TODO rafce
 
+import Ref from "./components/Ref";
+
+// import LayoutEffect from "./components/LayoutEffect";
+
 // import React from "react";
-import Effect from "./components/Effect";
+// import Effect from "./components/Effect";
+
 // import { State } from "./components/State";
 // import Calculadora from "./components/Calculadora";
 
+//* COMPONENTES COMENTADOS
+// <Effect />
+// <LayoutEffect />;
 const App = () => {
   return (
     <div className="container text-center">
-      <Effect />
+      <Ref />
     </div>
     // <div className="container text-center">
     //   <h1>Calculadora - PWA</h1>
