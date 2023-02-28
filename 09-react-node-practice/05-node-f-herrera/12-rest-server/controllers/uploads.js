7d864da9127da22e47a15db27700c20793e0b77c
glinares6 <glinarese2@gmail.com>
2023-02-28 08:00:19 +0100

    uplodad - preview dev - 2.0.0 node

--- 09-react-node-practice/05-node-f-herrera/12-rest-server/controllers/uploads.js
+++ 09-react-node-practice/05-node-f-herrera/12-rest-server/controllers/uploads.js
@@ -1,15 +1,18 @@
 const { response } = require("express");
 const { subirArchivo } = require("../helpers");
 
+const { Usuario, Producto } = require("../models");
 const cargarArchivo = async (req, res = response) => {
   // let sampleFile;
   // let uploadPath;
 
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-    // res.status(400).send("No hay archivos que subir");
-    res.status(400).json("No hay archivos que subir");
-    return;
-  }
+  //*enviado al middleware validar-archivo
+  // if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+  //   // res.status(400).send("No hay archivos que subir");
+  //   res.status(400).json({ msg: "No hay archivos que subir" });
+  //   return;
+  // }
+
   //   console.log("req.files >>>", req.files); // eslint-disable-line
 
   //txt md
@@ -28,6 +31,47 @@ const cargarArchivo = async (req, res = response) => {
   }
 };
 
+const actualizarImagen = async (req, res = response) => {
+  //*Ç enviado al middleware validar-archivo
+  // if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+  //   res.status(400).json({ msg: "No hay archivos que subir" });
+  //   return;
+  // }
+
+  const { id, coleccion } = req.params;
+
+  let modelo;
+
+  switch (coleccion) {
+    case "usuarios":
+      modelo = await Usuario.findById(id);
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe un usuario con el id ${id}`,
+        });
+      }
+      break;
+    case "productos":
+      modelo = await Producto.findById(id);
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe un producto con el id ${id}`,
+        });
+      }
+      break;
+    default:
+      return res.status(500).json({ msg: "Se me olvidó validar esto " });
+  }
+
+  const nombre = await subirArchivo(req.files, undefined, coleccion);
+  modelo.img = nombre;
+
+  await modelo.save();
+  res.json(modelo);
+};
 module.exports = {
   cargarArchivo,
+  actualizarImagen,
 };
+
+//todo si no se envia el archivo se queda pegado como revolverlo ~ tarea
