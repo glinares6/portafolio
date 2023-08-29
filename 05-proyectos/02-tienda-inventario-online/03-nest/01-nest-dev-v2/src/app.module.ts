401d81e335b93378e1489ee959e247b1faae7e9b
glinares6 <glinarese2@gmail.com>
2023-08-29 08:58:21 +0200

    nest-proyv2.18

--- 05-proyectos/02-tienda-inventario-online/03-nest/01-nest-dev-v2/src/app.module.ts
+++ 05-proyectos/02-tienda-inventario-online/03-nest/01-nest-dev-v2/src/app.module.ts
@@ -3,10 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { PerModule } from './per/per.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [CatsModule, PerModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestCli',
+      entities: [User],
+      synchronize: true,
+    }),
+    CatsModule,
+    PerModule,
+    UsersModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
