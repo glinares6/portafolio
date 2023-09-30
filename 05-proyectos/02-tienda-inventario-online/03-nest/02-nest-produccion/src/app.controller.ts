a0dd582cdaca529606b6076cf025eb5911b851a7
glinares6 <glinarese2@gmail.com>
2023-09-30 22:49:06 +0200

    nest-next-desarrollo-v1.24

--- 05-proyectos/02-tienda-inventario-online/03-nest/02-nest-produccion/src/app.controller.ts
+++ 05-proyectos/02-tienda-inventario-online/03-nest/02-nest-produccion/src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
   // private readonly logger = new Logger(AppController.name);
 
-  @Cron('60 * * * * *')
+  @Cron('30 * * * * *')
   handleCron() {
     // this.logger.debug('Called when the current second is 2');
     fetch('https://nest-online-build.onrender.com/ping').then((response) =>
