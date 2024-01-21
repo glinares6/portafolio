6411d313c5e58f2c62ac93ec9ee63eef295e86fc
glinares6 <glinarese2@gmail.com>
2024-01-21 03:11:03 -0500

    nest-next-desarrollo-v1.87

--- 05-proyectos/02-tienda-inventario-online/02-tienda-inventario-frontend/04-tienda-inventario-produccion-cliente/src/app/components/@registrocliente/page.tsx
+++ 05-proyectos/02-tienda-inventario-online/02-tienda-inventario-frontend/04-tienda-inventario-produccion-cliente/src/app/components/@registrocliente/page.tsx
@@ -37,6 +37,7 @@ export default function RegistroCliente() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify(payloadRegistroCliente),
       credentials: "include",
