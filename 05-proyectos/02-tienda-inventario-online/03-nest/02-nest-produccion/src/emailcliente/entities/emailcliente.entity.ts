89b975df362c03fd5f32ce0863346e644cf782a7
glinares6 <glinarese2@gmail.com>
2024-01-21 02:44:47 -0500

    nest-next-desarrollo-v1.84

--- 05-proyectos/02-tienda-inventario-online/03-nest/02-nest-produccion/src/emailcliente/entities/emailcliente.entity.ts
+++ 05-proyectos/02-tienda-inventario-online/03-nest/02-nest-produccion/src/emailcliente/entities/emailcliente.entity.ts
@@ -1,1 +1,9 @@
-export class Emailcliente {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Emailcliente {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @Column({ default: '' })
+  emailcliente: string;
+}
