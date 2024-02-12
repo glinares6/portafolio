790f605bfa3931e0d2990e4bae9407a1a81ba0b6
glinares6 <glinarese2@gmail.com>
2024-02-12 09:48:07 -0500

    nest-next-desarrollo-v1.128

--- 05-proyectos/02-tienda-inventario-online/03-nest/02-nest-produccion/src/main.ts
+++ 05-proyectos/02-tienda-inventario-online/03-nest/02-nest-produccion/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import { jwtConstants } from './auth/constansts';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 // import * as express from 'express';
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule{cors: true,});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.use(
     session({
       secret: jwtConstants.secret,
@@ -18,11 +22,11 @@ async function bootstrap() {
 
   // app.use(express.urlencoded({ extended: true, limit: '10mb' }));
   // app.use(express.json({ limit: '10mb' }));
-  app.enableCors({
-    origin: true, // Reemplaza con tu dominio de túnel
-    methods: 'GET,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: true, // Reemplaza con tu dominio de túnel
+  //   methods: 'GET,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
 
   await app.listen(port);
 }
