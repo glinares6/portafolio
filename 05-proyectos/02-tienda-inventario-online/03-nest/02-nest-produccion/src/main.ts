576fa5260d6db3652ef5a367dbefc9b6002389a6
glinares6 <glinarese2@gmail.com>
2024-02-12 22:39:20 -0500

    nest-next-desarrollo-v1.147

--- 05-proyectos/02-tienda-inventario-online/03-nest/02-nest-produccion/src/main.ts
+++ 05-proyectos/02-tienda-inventario-online/03-nest/02-nest-produccion/src/main.ts
@@ -8,18 +8,25 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(
     session({
       secret: jwtConstants.secret,
-      resave: true,
+      resave: false,
       saveUninitialized: true,
+      cookie: {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'none',
+        maxAge: 60 * 60 * 24 * 1000,
+      },
     }),
   );
 
   // app.use(express.urlencoded({ extended: true, limit: '10mb' }));
   // app.use(express.json({ limit: '10mb' }));
   app.enableCors({
-    origin: true, // Reemplaza con tu dominio de túnel
+    origin: 'null', // Reemplaza con tu dominio de túnel
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
